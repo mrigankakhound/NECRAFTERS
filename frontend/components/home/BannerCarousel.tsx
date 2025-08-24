9d9d78d812ac7cce5e6d0662aa5f572d819f0fd6
mrigankakhound <112114118ankur@gmail.com>
2025-08-24 20:49:37 +0530

    Remove refresh icon from banner carousel - real-time sync works automatically

--- frontend/components/home/BannerCarousel.tsx
+++ frontend/components/home/BannerCarousel.tsx
@@ -153,22 +153,6 @@ const BannerCarousel = ({ banners: initialBanners, app_banners: initialAppBanner
         <ChevronRight size={24} />
       </Button>
       
-      {/* Manual refresh button */}
-      <Button
-        variant={"outline"}
-        size={"icon"}
-        onClick={fetchBanners}
-        aria-label="Refresh banners"
-        className="absolute top-4 right-4 bg-white/80 hover:bg-white text-black rounded-full"
-        title="Refresh banners"
-      >
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/>
-          <path d="M21 3v5h-5"/>
-          <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/>
-          <path d="M3 21v-5h5"/>
-        </svg>
-      </Button>
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {images.map((_, index) => (
           <button
